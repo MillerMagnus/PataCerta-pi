762db1128538770370c81aa33f7570d0c2e39d01
koharu <luizfelipepereirachagas2019@gmail.com>
2026-09-11 20:28:47 -0300

    errr

--- script.js
+++ script.js
@@ -47,7 +47,10 @@ const animais = [
     imagem: ""
   },
 ];
- const lista = document.querySelector("#cardpet");
+
+/*----------------------------------------------------------------------------------------*/
+
+const listaCardpet = document.querySelector("#cardpet");
   animais.forEach(animal => {
     const article = document.createElement ("article");
     article.dataset.id = animal.id;
@@ -56,9 +59,26 @@ const animais = [
         <h2>${animal.nome}</h2>
         <p>${animal.especie}</p>
          <p>${animal.idade}</p>
-          <p>${animal.porte}</p>
-            <p>${animal.sexo}</p>
-            <p>${animal.cidade}</p>
+            <p>${animal.status}</p>
+            <a href="perfilpet.html?id=${animal.id}">
+            <button> Ver detalhes</button>
+            </a>
+            `;
+            listaCardpet.appendChild(article);
+  });
+
+  const listaCatalogoPet = document.querySelector("#perfilpet");
+  animais.forEach(animal => {
+    const article = document.createElement ("article");
+    article.dataset.id = animal.id;
+    article.innerHTML = `
+    <img src = "${animal.imagem}" alt ="${animal.nome}">
+        <h2>${animal.nome}</h2>
+        <p>${animal.especie}</p>
+        <p>${animal.idade}</p>
+           <p>${animal.porte}</p> 
+              <p>${animal.sexo}</p>
+              <p>${animal.cidade}</p>
             <p>${animal.status}</p>
             <a href="perfilpet.html?id=${animal.id}">
             <button> Ver detalhes</button>
