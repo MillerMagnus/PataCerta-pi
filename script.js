dc1d244bd5d640f4397a58810715c44de62e5ad9
MillerMagnus <millermagnus@outlook.com>
2026-09-16 20:27:11 -0300

    fav fix

--- script.js
+++ script.js
@@ -68,8 +68,7 @@ const listaCardpet = document.querySelector("#cardpet");
             </a>
             <label class="favoritar"> 
             <input type="checkbox" name="favoritar" value="${animal.id}">
-              <span class="slider round"></span>
-              <span class="slider"></span>
+              <span class="checkmark"></span>
             </label>
             `;
             listaCardpet.appendChild(article);
